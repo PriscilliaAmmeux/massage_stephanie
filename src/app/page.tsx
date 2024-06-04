599cb28a840d3ebef380a4370d773ac1780f0f68
Priscillia <selvapriscillia@yahoo.fr>
2024-06-04 15:26:40 +0200

    add info in meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import { Metadata } from "next";
 import Header from "./ui/header/header";
 import Presentation from "./ui/presentation/presentation";
 import Prestations from "./ui/prestations/prestations";
 import Footer from "./ui/footer/footer";
 import Qvt from "./ui/qvt/qvt";
 import backgroundImage from "../../public/feather2.jpg";
-
-export const metadata: Metadata = {
-  title: "Stéphanie Heudre - Massage bien-être à domicile et au travail",
-  description:
-    "Découvrez les services de massage bien-être proposés par Stéphanie Heudre, spécialiste en qualité de vie au travail (CQVT). Profitez de massages relaxants et thérapeutiques directement à votre domicile ou sur votre lieu de travail, pour une détente optimale et une amélioration de votre bien-être quotidien.",
-};
 
 export default function Home() {
   return (
