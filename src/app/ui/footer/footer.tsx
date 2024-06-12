4d4c33836fac1d182290055b6865666b71955bee
Priscillia <selvapriscillia@yahoo.fr>
2024-06-12 20:48:56 +0200

    test deployed

--- src/app/ui/footer/footer.tsx
+++ src/app/ui/footer/footer.tsx
@@ -5,11 +5,11 @@ export default function Footer() {
     <footer className="w-full bg-pink-100  sm:p-6 md:p-10 flex flex-col items-center justify-center pt-10 px-4 gap-2 text-center md:text-left text-sm">
       <p className="italic flex flex-col md:flex-row gap-2 items-center">
         Créé par{" "}
-        <span className="font-bold text-pink-700">Priscillia Ammeux</span>{" "}
+        <span className="font-bold text-pink-700">Priscillia Ammeux</span>
         <GiLadybug className="text-pink-700" />
       </p>
       <p className="italic">
-        Conceptrice et Développeuse d&apos;Applications:{" "}
+        Conceptrice et Développeuse d&apos;Applications:
         <a
           href="http://www.priscillia-ammeux-portfolio.fr"
           className="text-pink-700 hover:underline"
