0bdefb8579cf68c88a61764292bbb4b3b1dc342c
Priscillia <selvapriscillia@yahoo.fr>
2024-06-13 09:44:02 +0200

    change color footer for contraste and acceesibility

--- src/app/ui/footer/footer.tsx
+++ src/app/ui/footer/footer.tsx
@@ -5,14 +5,14 @@ export default function Footer() {
     <footer className="w-full bg-gray-800 text-white sm:p-6 md:p-10 flex flex-col items-center justify-center pt-4 pb-4 px-4 gap-2 text-center md:text-left text-sm">
       <p className="italic flex flex-col md:flex-row gap-2 items-center">
         Créé par{" "}
-        <span className="font-bold text-pink-200">Priscillia Ammeux</span>
-        <GiLadybug className="text-pink-200" />
+        <span className="font-bold text-pink-100">Priscillia Ammeux</span>
+        <GiLadybug className="text-pink-100" />
       </p>
-      <p className="italic">
+      <p>
         Conceptrice et Développeuse d&apos;Applications:
         <a
           href="http://www.priscillia-ammeux-portfolio.fr"
-          className="pl-2 text-pink-200 hover:underline hover:text-pink-700"
+          className="pl-2 text-pink-100 hover:underline hover:font-semibold"
           target="_blank"
           rel="noopener noreferrer">
           www.priscillia-ammeux-portfolio.fr
