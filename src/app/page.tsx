4c29de747ce8c1d70d0b17bfdae71e483b6b55bd
Priscillia <selvapriscillia@yahoo.fr>
2024-06-13 09:56:07 +0200

    update meta and change header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import SocialNetwork from "./ui/socialNetwork/socialNetwork";
 export const metadata: Metadata = {
   title: "Stéphanie Heudre - Massage bien-être à domicile et au travail",
   description:
-    "Découvrez les services de massage bien-être proposés par Stéphanie Heudre, spécialiste en qualité de vie au travail (CQVT). Profitez de massages relaxants et thérapeutiques directement à votre domicile ou sur votre lieu de travail, pour une détente optimale et une amélioration de votre bien-être quotidien. Stéphanie intervient en entreprise et propose des massages Chi Nei Tsang, inspirés de la médecine chinoise, pour une sérénité et un zen total. Que ce soit pour un RDV à domicile ou au SPA, votre bien-être et votre sérénité sont au cœur de ses interventions. Engagez-vous dans une démarche de RSE en améliorant la QVCT grâce à ces services de massage et de bien-être. Déplacements assurés pour des séances de détente et de bien-être.",
+    "Découvrez les services de massage bien-être proposés par Stéphanie Heudre, infirmière diplômée d'état et spécialiste en qualité de vie au travail. Profitez de massages relaxants et thérapeutiques à domicile ou sur votre lieu de travail. Stéphanie intervient également en entreprise pour des journées QVT, et propose des massages inspirés de la médecine chinoise, comme le Chi Nei Tsang, pour une détente optimale. Engagez-vous dans une démarche de RSE en améliorant la QVCT grâce à ces services de massage et de bien-être.",
 };
 
 export default function Home() {
