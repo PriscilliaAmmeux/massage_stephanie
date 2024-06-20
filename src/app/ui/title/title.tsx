22eff12b6d7c898f5b7ed5e28300218d846d7c67
Priscillia <selvapriscillia@yahoo.fr>
2024-06-20 21:41:45 +0200

    increase size title

--- src/app/ui/title/title.tsx
+++ src/app/ui/title/title.tsx
@@ -8,7 +8,7 @@ interface TitleProps {
 
 const Title: React.FC<TitleProps> = ({ title, Icon }) => (
   <div className="flex items-center mb-4">
-    <Icon className="text-pink-700 text-xl mr-2" />
+    <Icon className="text-xl text-pink-700 mr-2" />
     <h1 className="text-xl text-pink-700 font-bold">{title}</h1>
   </div>
 );
