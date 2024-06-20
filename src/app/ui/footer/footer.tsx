a21cb11c7537a1c1c80139a08aac650b37715085
Priscillia <selvapriscillia@yahoo.fr>
2024-06-20 21:46:08 +0200

    fix the footer in column and move my info to bottom

--- src/app/ui/footer/footer.tsx
+++ src/app/ui/footer/footer.tsx
@@ -1,8 +1,14 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-col md:flex-row gap-20 w-full bg-gray-800 text-white sm:p-6 md:p-10 flex flex-col items-center justify-center pt-4 pb-4 px-4 gap-2 text-center md:text-left text-sm">
+    <footer className="flex flex-col items-center gap-6 w-full bg-gray-800 text-white sm:p-6 md:p-10 flex flex-col items-center justify-center pt-4 pb-4 px-4 gap-2 text-center md:text-left text-sm">
+      <section className="flex flex-col  items-center space-y-2">
+        <p>© 2024 Stéphanie Heudre. Tous droits réservés. </p>
+        <p className="underline">Mentions légales: </p>
+        <p>Siret de l&apos;établissement: 85127227800041</p>
+      </section>
+      <div className="text-2xl">~</div>
       <section>
-        <p className="flex flex-col md:flex-row gap-2 items-center">
+        <p className="flex flex-col space-y-2 items-center">
           Créé par <span className="font-bold ">Priscillia Ammeux</span>
         </p>
         <p>
@@ -15,12 +21,6 @@ export default function Footer() {
             www.priscillia-ammeux-portfolio.fr
           </a>
         </p>
-      </section>
-
-      <section className="space-y-2">
-        <p>© 2024 Stéphanie Heudre. Tous droits réservés. </p>
-        <p className="underline">Mentions légales: </p>
-        <p>Siret de l&apos;établissement: 85127227800041</p>
       </section>
     </footer>
   );
