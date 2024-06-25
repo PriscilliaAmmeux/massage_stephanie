8d4ccbfc732215bf3a082f12a928f083ca20a965
Priscillia <selvapriscillia@yahoo.fr>
2024-06-25 16:20:14 +0200

    add rgpd modal

--- src/app/ui/footer/footer.tsx
+++ src/app/ui/footer/footer.tsx
@@ -1,17 +1,23 @@
 import Logo from "../logo/logo";
 
-export default function Footer() {
+export default function Footer({ onRgpdClick }: { onRgpdClick: () => void }) {
   return (
     <footer className="flex flex-col md:flex-row items-center gap-6 w-full bg-gray-800 text-white sm:p-6 md:p-10 flex flex-col items-center justify-center pt-4 pb-4 px-4 gap-2 text-center md:text-left text-sm">
       <Logo width={200} height={200} />
-      <div>
-        <section className="flex flex-col  items-center space-y-2">
+      <section>
+        <div className="flex flex-col  items-center space-y-2">
           <p>© 2024 Stéphanie Heudre. Tous droits réservés. </p>
           <p className="underline">Mentions légales: </p>
           <p>Siret de l&apos;établissement: 85127227800041</p>
-        </section>
+          <p
+            className="font-bold hover:underline"
+            onClick={onRgpdClick}
+            style={{ cursor: "pointer" }}>
+            Politiques de confidentialité
+          </p>
+        </div>
         <div className="flex justify-center text-2xl">~</div>
-        <section>
+        <div>
           <p className="flex flex-col space-y-2 items-center">
             Créé par <span className="font-bold ">Priscillia Ammeux</span>
           </p>
@@ -25,8 +31,8 @@ export default function Footer() {
               www.priscillia-ammeux-portfolio.fr
             </a>
           </p>
-        </section>
-      </div>
+        </div>
+      </section>
     </footer>
   );
 }
