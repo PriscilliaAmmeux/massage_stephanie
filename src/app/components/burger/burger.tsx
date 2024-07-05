b99deb413e616599f929b1812e775e5732f74b6a
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2024-07-05 13:14:22 +0200

    fix height header mobile (#113)
    * fix height header mobile
    
    * header is no longer displayed for cell phones in landscape mode
    
    ---------
    
    Co-authored-by: Priscillia <selvapriscillia@yahoo.fr>

--- src/app/components/burger/burger.tsx
+++ src/app/components/burger/burger.tsx
@@ -65,16 +65,22 @@ export default function Burger() {
           } flex flex-col items-center justify-start `}
           onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the menu
         >
-          <div className="flex justify-around items-center mb-5 mr-5 ml-5 ">
-            <span className="mr-5">
+          <div
+            className="flex justify-around items-center mt-28
+           mb-5 mr-5 ml-5 ">
+            <span>
               <Logo theme="dark" width={50} height={50} />
             </span>
             <div className="flex flex-col items-center ">
-              <h1 className="text-2xl font-bold">Stéphanie Heudre</h1>
-              <h2 className="text-xl">Praticienne bien-être certifiée</h2>
+              <h1 className="text-2xl font-bold items-center">
+                Stéphanie Heudre
+              </h1>
+              <h2 className="text-xl items-center text-center">
+                Praticienne bien-être certifiée
+              </h2>
             </div>
             <button onClick={handleCloseMenu}>
-              <FaTimes className="cursor-pointer ml-5 " />
+              <FaTimes className="cursor-pointer" />
             </button>
           </div>
 
