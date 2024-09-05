25e53b0c08df219f9375a2dab2511363edc67e4d
Priscillia <selvapriscillia@yahoo.fr>
2024-09-05 16:35:39 +0200

    delete popup old rgpd

--- src/app/ui/layout/layout.tsx
+++ src/app/ui/layout/layout.tsx
@@ -1,32 +1,21 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import Footer from "../footer/footer";
 import Header from "../header/header";
 import Navigation from "../navigation/navigation";
-import RgpdModal from "../rgpdModal/rgpdModal";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-  const [isRgpdModalOpen, setRgpdModalOpen] = useState(false);
-
-  const handleRgpdClick = () => {
-    setRgpdModalOpen(true);
-  };
-
-  const closeRgpdModal = () => {
-    setRgpdModalOpen(false);
-  };
   return (
     <section>
       <Header className="hidden md:block" />
       <Navigation />
       {children}
-      <Footer onRgpdClick={handleRgpdClick} />
-      {isRgpdModalOpen && <RgpdModal onClose={closeRgpdModal} />}
+      <Footer />
     </section>
   );
 }
