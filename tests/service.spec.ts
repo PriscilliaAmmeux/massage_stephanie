f040e57465fef1a71cb34c282b931d64341e69d3
Priscillia <selvapriscillia@yahoo.fr>
2024-09-06 13:52:36 +0200

    fix error test playwright

--- tests/service.spec.ts
+++ tests/service.spec.ts
@@ -2,4 +2,27 @@ import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await page.goto("https://www.massage-stephanie-heudre.fr/prestations");
+  await page
+    .locator("#prestations")
+    .getByRole("heading", { name: "Prestations" })
+    .click();
+  await page
+    .getByRole("heading", { name: "Je me déplace dans les villes" })
+    .click();
+  await page
+    .getByRole("heading", { name: "Qualité de Vie au Travail (" })
+    .click();
+  await page.getByText("Pourquoi choisir ce service ?").click();
+  await page.getByText("Contactez-moi dès aujourd'hui").click();
+  await page.getByLabel("Envoyer un email").click();
+  await page.getByRole("link", { name: "Prestations" }).click();
+
+  await page
+    .locator("footer")
+    .getByRole("img", { name: "logo SH pour Stéphanie Heudre" })
+    .click();
+  await page.getByRole("link", { name: "Mentions légales" }).click();
+  const page12Promise = page.waitForEvent("popup");
+  await page.getByRole("link", { name: "www.priscillia-ammeux-" }).click();
+  const page12 = await page12Promise;
 });
