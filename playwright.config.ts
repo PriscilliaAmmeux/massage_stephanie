a90e60797a8fad9671c1efdde6917064426d6483
Priscillia <selvapriscillia@yahoo.fr>
2024-09-06 12:22:42 +0200

    fix error test playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,6 +43,11 @@ export default defineConfig({
       use: { ...devices["Desktop Firefox"] },
     },
 
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
--- tests/service.spec.ts
+++ tests/service.spec.ts
@@ -2,19 +2,26 @@ import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await page.goto("https://www.massage-stephanie-heudre.fr/prestations");
-  await page.getByRole("heading", { name: "Massage Assis ou AMMA" }).click();
-  await page.getByRole("heading", { name: "Chi Nei Tsang" }).click();
-  await page.getByRole("heading", { name: "Réfléxologie crânienne" }).click();
-  await page
-    .getByRole("heading", { name: "Je me déplace dans les villes" })
-    .click();
-  await page
-    .getByRole("heading", { name: "Qualité de Vie au Travail (" })
-    .click();
+  await page.getByRole("link", { name: "Contact" }).click();
+  await page.getByRole("link", { name: "Chi Nei Tsang" }).click();
+  await page.getByRole("link", { name: "Prestations" }).click();
+  await page.getByRole("link", { name: "Réfléxologie crânienne" }).click();
+  await page.getByRole("link", { name: "Prestations" }).click();
+  await page.getByRole("link", { name: "Qualité de Vie au Travail" }).click();
+  await page.getByRole("link", { name: "Massage Assis ou AMMA" }).click();
+  await page.getByRole("link", { name: "Réfléxologie crânienne" }).click();
+  await page.getByRole("link", { name: "Qualité de Vie au Travail" }).click();
   await page.getByLabel("Envoyer un email").click();
-  await page.getByLabel("Fermer").click();
-  await page
-    .locator("footer")
-    .getByRole("img", { name: "logo SH pour Stéphanie Heudre" })
-    .click();
+  await page.getByRole("link", { name: "Prestations" }).click();
+  await page.getByRole("link", { name: "Mentions légales" }).click();
+  await page.getByRole("link", { name: "Prestations" }).click();
+  const page3Promise = page.waitForEvent("popup");
+  await page.getByRole("link", { name: "www.priscillia-ammeux-" }).click();
+  const page3 = await page3Promise;
+  const page4Promise = page.waitForEvent("popup");
+  await page.getByLabel("Link to Facebook profile").click();
+  const page4 = await page4Promise;
+  const page5Promise = page.waitForEvent("popup");
+  await page.getByLabel("Link to LinkedIn profile").click();
+  const page5 = await page5Promise;
 });
