dbdae4f910fffe4e7b3dff2fde7900634af5e285
Priscillia <selvapriscillia@yahoo.fr>
2025-10-07 21:53:16 +0200

    update footer

--- src/app/ui/footer/footer.tsx
+++ src/app/ui/footer/footer.tsx
@@ -3,11 +3,13 @@ import Logo from "../logo/logo";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="flex flex-col md:flex-row items-center gap-6 w-full bg-gray-700 text-white sm:p-6 md:p-10 flex flex-col items-center justify-center pt-4 pb-4 px-4 gap-2 text-center md:text-left text-sm">
+    <footer className="flex flex-col md:flex-row items-center gap-6 w-full bg-gray-700 text-white sm:p-6 md:p-10 pt-4 pb-4 px-4 gap-2 text-center md:text-left text-sm justify-center">
       <Logo width={200} height={200} theme={"light"} />
       <section>
-        <div className="flex flex-col  items-center space-y-2">
-          <p>© {currentYear} Stéphanie Heudre. Tous droits réservés. </p>
+        <div className="flex flex-col items-center space-y-2">
+          <p className="font-bold">
+            © {currentYear} Stéphanie Heudre. Tous droits réservés.
+          </p>
           <a
             href="/legalNotices"
             className="hover:text-pink-700 hover:font-bold cursor-pointer">
@@ -20,20 +22,18 @@ export default function Footer() {
           </a>
         </div>
         <div className="flex justify-center text-xl">~</div>
-        <div>
-          <p className="flex flex-col space-y-2 items-center">
-            Créé par <span className="font-bold ">Priscillia Ammeux</span>
-          </p>
-          <p>
-            Conceptrice et Développeuse d&apos;Applications:
+        <div className="flex justify-center space-y-1 mt-2">
+          <span>
+            Créé par{" "}
             <a
-              href="http://www.priscillia-ammeux-portfolio.fr"
-              className="pl-2 hover:underline hover:font-semibold"
+              href="https://www.pixelia-and-co.fr/"
+              className="font-bold hover:underline hover:font-semibold text-pink-300"
               target="_blank"
               rel="noopener noreferrer">
-              www.priscillia-ammeux-portfolio.fr
-            </a>
-          </p>
+              Pixelia & Co
+            </a>{" "}
+            – Agence web
+          </span>
         </div>
       </section>
     </footer>
